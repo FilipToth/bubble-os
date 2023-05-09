9aa6405c1bffdca969bb2aec3cb7edf6efa20e99
Filip Toth <filip.toth@accelley.com>
2023-05-09 03:52:44 -0400

    Add python script to run qemu and separate ovmf path to .env file

---
+++ qemu.py
@@ -0,0 +1,33 @@
+import os
+import dotenv
+
+def run_qemu():
+    if not os.path.exists('.env'):
+        print('.env file not found')
+        return
+
+    dotenv.load_dotenv()
+    env_vars = dotenv.dotenv_values('.env')
+
+    if not env_vars.__contains__('OVMF_PATH'):
+        print('OVMF_PATH not set in .env file')
+        return
+
+    ovmf_path = env_vars['OVMF_PATH']
+    if not os.path.exists(ovmf_path):
+        print('OVMF_PATH does not exist')
+        return
+    
+    os.system('cargo build')
+    
+    os.system(f"""
+        qemu-system-x86_64 \
+            -enable-kvm \
+            -m 512 \
+            -nographic \
+            -bios {ovmf_path} \
+            -device driver=e1000,netdev=n0 \
+            -netdev user,id=n0,tftp=target/x86_64-unknown-uefi/debug,bootfile=bubble-os.efi
+    """)
+
+run_qemu()
